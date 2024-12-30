5e502daac2e923aafc777e0723d213170a2b4e2d
shubham mishra <shubhammishra@shubhams-MacBook-Air.local>
2024-12-31 00:08:25 +0530

    pROJECT complate

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+let number = Math.trunc(Math.random() * 20);
+let score = 20;
+let highScore = 0;
+
+console.log(number);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number';
+  } else if (guess < number) {
+    document.querySelector('.message').textContent = '🔻 Your Guess is too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+    console.log(score);
+  } else if (guess > number) {
+    document.querySelector('.message').textContent =
+      '📈 Your Guess is too High';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent =
+      "✅ Hurrayyyy Bubu you guess it right & All Dudu's money is yours";
+    // document.querySelector('.number').textContent = number;
+    document.querySelector('.img').src = '/assets/bubuWon.png';
+    document.querySelector('.img').style.width = '80%';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('.number').style.background = 'white';
+    document.querySelector('.number').style.color = 'white';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  // Reset Message
+  document.querySelector('.message').textContent = 'Start guessing...';
+  // Reset input Guess
+  document.querySelector('.guess').value = '';
+  // Reset ? Number
+  // document.querySelector('.number').textContent = '?';
+  document.querySelector('.img').src = '/assets/bubuThink.png';
+  document.querySelector('.number').style.background = 'white';
+  document.querySelector('.number').style.color = '#222';
+  document.querySelector('.score').textContent = 20;
+  number = Math.trunc(Math.random() * 20);
+  console.log(number);
+  score = 20;
+});
